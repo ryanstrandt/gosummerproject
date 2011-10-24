b333f9b22cb11138e8dbc279036e01b1d5823d62
Ryan Strandt <ryan.strandt@gmail.com>
2011-10-24 12:36:13 -0400

    - google maps on projects page - facebook javascript file in application layout

---
+++ public/javascripts/projects.map.js
@@ -0,0 +1,34 @@
+// Global variable
+var map;
+// Initialize Google Map
+function init() {
+	// Draw the map (blank)
+	var map_focus = new google.maps.LatLng(35,-37);
+	var myOptions = { zoom: 2,center: map_focus,mapTypeId: google.maps.MapTypeId.ROADMAP };  
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	$('#map_overlay').hide();
+	$('#map_overlay').css('visibility', 'visible');
+}
+
+// Parse XML of project locations to create map markers
+function getMarkers() {
+	// Check session variable 'parsed' to see if markers have already been parsed
+	$.ajax({
+		url: "../get_status.php",
+		success: function(status){
+			eval(status);
+			// If markers haven't been parsed, display loading overlay
+			if( $.trim(parsed) != "true" ) {
+				$('#map_overlay').fadeIn(500);
+			}
+		}
+	});
+	// Generate marker code
+	$.ajax({
+		url: "../get_locations.php",
+		success: function(markers){
+			eval(markers);
+			$('#map_overlay').fadeOut(500);
+		}
+	});
+}
