9bc33e77d893a0f1c9251699000804713675c2e4
Ryan Strandt <ryan.strandt@gmail.com>
2011-10-24 13:45:42 -0400

    - removed galleria - explore projects links work when going back to home

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,6 @@
 $(function(){
 	// Set starting slide to 1
 	var startSlide = 1;
-	// Get slide number if it exists
-	if (window.location.hash) {
-		startSlide = window.location.hash.replace('#','');
-	}
 	// Initialize Slides
 	$('#slides').slides({
 		preload: true,
